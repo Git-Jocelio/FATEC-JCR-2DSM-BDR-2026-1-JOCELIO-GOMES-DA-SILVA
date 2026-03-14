22a078f7fe3d5e9276a5fb963da30d3f740beeee
Git-Jocelio <trelicas.aricanga@gmail.com>
2026-03-13 21:05:06 -0300

    Add files via upload

---
+++ P1-Conteudos/Atividades/Atividade-Aula05/atividade aula 5.sql
@@ -0,0 +1,30 @@
+/*atividade 5*/
+insert into localizacao (latitude, longitude, cidade, estado)
+values ('25.2555','-36.452','São Paulo','SP')
+insert into localizacao (latitude, longitude, cidade, estado)
+values ('85.2555','-72.452','Rio de Janeiro','RJ')
+insert into localizacao (latitude, longitude, cidade, estado)
+values ('28.4755','-85.477','Pernambuco','PE')
+
+select * from localizacao
+
+insert into usuario (nome, email, senhahash)
+values ('Leticia','leticia@teste.com','HSQ12654DEE')
+insert into usuario (nome, email, senhahash)
+values ('Renato','renato@teste.com','HSQsdsdaDLL')
+insert into usuario (nome, email, senhahash)
+values ('Luzia','luzia@teste.com','HSQ1DSDSDS')
+
+SELECT * FROM USUARIO
+
+insert into tipoevento (nome, descricao)
+values ('Chuva torrencial','Chuva forte com potencial grande')
+insert into tipoevento (nome, descricao)
+values ('Calor','Calor com temperaturas amenas')
+insert into tipoevento (nome, descricao)
+values ('Neve','Neve com incidência moderada')
+
+select * from tipoevento
+
+
+
